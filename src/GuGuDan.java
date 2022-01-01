1e45ac35b0a9fdf6e6404d6e0b355bd082e74dd3
heegyun <yeom@dju.kr>
2022-01-01 18:58:21 +0900

    add file

---
+++ src/GuGuDan.java
@@ -0,0 +1,33 @@
+public class GuGuDan {
+
+    public void dan(int n) {
+        System.out.println(n*1);
+        System.out.println(n*2);
+        System.out.println(n*3);
+        System.out.println(n*4);
+        System.out.println(n*5);
+        System.out.println(n*6);
+        System.out.println(n*7);
+        System.out.println(n*8);
+        System.out.println(n*9);
+    }
+
+    public static void main(String[] args) {
+
+/*        int n = 2;
+        System.out.println(n*1);
+        System.out.println(n*2);
+        System.out.println(n*3);
+        System.out.println(n*4);
+        System.out.println(n*5);
+        System.out.println(n*6);
+        System.out.println(n*7);
+        System.out.println(n*8);
+        System.out.println(n*9);*/
+
+        int n = 3;
+        GuGuDan gugudan = new GuGuDan();
+        gugudan.dan(n);
+    }
+
+}
