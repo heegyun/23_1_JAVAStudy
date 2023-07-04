82dd2a8dde4d03175a6298eb17fb19fcb8c87aef
heegyun <yhg1124@daum.net>
2023-07-04 16:59:15 +0900

    Add files via upload

---
+++ src/my/day03/ForTest2.java
@@ -0,0 +1,24 @@
+package day03;
+
+public class ForTest2 {
+    public static void main(String[] args) {
+        // 1부터 10까지의 총합을 구하는 코드
+        int sum = 0;
+        sum = sum +1 ;
+        sum = sum +2 ;
+        sum = sum +3 ;
+        sum = sum +4 ;
+        sum = sum +5 ;
+        sum = sum +6 ;
+        sum = sum +7 ;
+        sum = sum +8 ;
+        sum = sum +9 ;
+        sum +=10 ;
+        System.out.printf("총합: %d", sum);
+
+        // for 문으로  개선하면?
+
+
+    }
+}
+
