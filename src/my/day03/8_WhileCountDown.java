1e3eacdca1a7b2f512a0311d452c6a5fc10a399e
heegyun <yhg1124@daum.net>
2023-07-04 17:20:36 +0900

    Update 8_WhileCountDown.java

--- src/my/day03/8_WhileCountDown.java
+++ src/my/day03/8_WhileCountDown.java
@@ -3,10 +3,11 @@ public class WhileCountDown {
     public static void countdown(int num){
         System.out.println("카운트 다운을 시작합니다.");
 
-        while(num > 4){
+        while(){
             System.out.printf("%d..\n", num);
-            num--;
+           //  ;
         }
+        System.out.println("발사");
     }
     public static void main(String[] args) {
         WhileCountDown.countdown(5); // 반복 진행시 num의 값은 5부터 차례로 4,,3,2,1,0으로 나온다는 것을 유추하면
