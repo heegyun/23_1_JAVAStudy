c8cdf2e1e70c87c454cb50ab62b1b22941dff51f
heegyun <yhg1124@daum.net>
2023-07-08 13:03:53 +0900

    Delete Knight.java

--- src/my/day08/Knight.java
+++
@@ -1,9 +0,0 @@
-package my.day08;
-
-public class Knight extends Novice {
-    int stamina;//기력
-
-    public void slash(){// 베기
-        System.out.printf("%s(HP: %d, STAMINA: %d)의 슬래쉬!!\n",name, hp, stamina);
-    }
-}
