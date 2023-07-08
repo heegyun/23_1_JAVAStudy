87b64d5d926d4a40c7a7b77f23c42ae478659a23
heegyun <yhg1124@daum.net>
2023-07-08 13:06:23 +0900

    Delete CarTest.java

--- src/my/day08/CarTest.java
+++
@@ -1,9 +0,0 @@
-package my.day08;
-
-public class CarTest {
-    public static void main(String[] args) {
-        Truck truck = new Truck("봉고",200);
-        truck.load();
-        truck.move();
-    }
-}
