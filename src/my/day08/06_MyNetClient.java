9704286e62b4da857f499a733f422a7bb1aa6aec
heegyun <yhg1124@daum.net>
2023-07-11 18:11:18 +0900

    Update 06_MyNetClient.java

--- src/my/day08/06_MyNetClient.java
+++ src/my/day08/06_MyNetClient.java
@@ -1,20 +1,19 @@
 class  MyNetClient{
-	public static void main(String[] args)
-		throws Exception{
-		//클라이언트에서는 Socket만 필요...
-		//서버의 IP주소와 port번호가 필요
-		Socket sock=new Socket("172.16.10.207",5000);		
-		//연결되면 Socket객체가 생성되고...연결 안되면 예외 발생
-		System.out.println("서버와 연결됨...");	
-		InputStream is=sock.getInputStream();
-		DataInputStream dis=new DataInputStream(is);
-		String data=dis.readUTF();
-		System.out.println("서버로부터>>"+data);
-		int num=dis.readInt();
-		System.out.println("서버로부터>>"+num);
-		dis.close();
-		is.close();
-		sock.close();
+	public static void main(String[] args) {
+		try {
+			Socket s = new Socket("127.0.0.1",5000);
+			System.out.println("## 클라이언트 실행..");
+
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
+			
+			pw.println("안녕하세요? 클라이언트에서 전송합니다.!!");
+			pw.close();
+			s.close();
+			System.out.println("## 클라이언트 종료..");
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}//main()-----------------
 }////////////////////////////////////////////////////
 
