2a51aabae48ce7bc54a9bdd0165849c80bf89a85
heegyun <yhg1124@daum.net>
2023-07-11 17:44:16 +0900

    Update and rename NetTest.java to InetAddressTest.java

---
+++ src/my/day08/InetAddressTest.java
@@ -0,0 +1,26 @@
+package day08;
+//InetAddress클래스 사용:ip를 추상화한 클래스
+public class InetAddressTest {
+    public static void main(String[] args) {
+        InetAddress inet=null;
+		try{
+			inet=InetAddress.getByName(args[0]);
+			System.out.println("연결 됐습니다.");
+			System.out.println("getHostName: "+inet.getHostName());
+			System.out.println("getHostAddress: "
+								+inet.getHostAddress());
+			System.out.println("getLocalHost: "+inet.getLocalHost());
+
+			InetAddress[] inet2
+				=InetAddress.getAllByName("www.daum.net");
+
+			for (int i=0;i<inet2.length ;i++ )
+			{
+				System.out.println(inet2[i]);
+			}
+
+		}catch(UnknownHostException e){
+			e.printStackTrace();
+		}	
+    }
+}
--- src/my/day08/NetTest.java
+++
@@ -1,7 +0,0 @@
-package day08;
-
-public class NetTest {
-    public static void main(String[] args) {
-
-    }
-}
