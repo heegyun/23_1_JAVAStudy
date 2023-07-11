a9806dca7a90855b7a3b8e1b71bfaf40bfc6f0c0
heegyun <yhg1124@daum.net>
2023-07-11 18:15:43 +0900

    Update 07_MyNetServer.java

--- src/my/day08/07_MyNetServer.java
+++ src/my/day08/07_MyNetServer.java
@@ -1,28 +1,19 @@
 /*TCP통신 방식에서 서버단에서는 ServerSocket과 Socket이 필요*/
 class  MyNetServer{
-	public static void main(String[] args) throws IOException{
-		//서버소켓: 포트 번호 필요...
-		int port=5000;
-		int myNum=Integer.parseInt(args[0]);
-		//1. 서버 소켓 생성-----
-		ServerSocket ser=new ServerSocket(port);
-		out.println("클라이언트 연결을 기다림...");
-		while(true){
-			Socket sock=ser.accept();
-			//클이 접속해서 들어오면 서버 소켓은 클과
-			//연결된 소켓객체를 반환해준다.
-		out.println("클이 접속해옴...");
-		out.println("클의 IP: "+sock.getInetAddress().getHostAddress());
-			//클과 통신하기 위해 스트림 연결
-			OutputStream os=sock.getOutputStream();
-			DataOutputStream dos=new DataOutputStream(os);
-			dos.writeUTF(args[1]);
-			dos.writeInt(myNum);
-			dos.flush();
-			dos.close();
-			sock.close();
-		}//while--------
-		//ser.close();
+	 public static void main(String[] args) {
+        try {
+            ServerSocket sc = new ServerSocket(5000);
+            System.out.println("## 서버 실행..");
+            while(true) {
+                Socket s = sc.accept();
+                BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-	}
+                System.out.println("## 클라이언트 연결됨 !!");
+                System.out.println("## 클라이언트 메시지: "+br.readLine());
+                br.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
