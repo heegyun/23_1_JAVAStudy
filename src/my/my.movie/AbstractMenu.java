bd0c1b92e77d6a5731a56dd48dc91394752226e4
heegyun <yhg1124@daum.net>
2023-07-11 19:16:58 +0900

    Update AbstractMenu.java

--- src/my/my.movie/AbstractMenu.java
+++ src/my/my.movie/AbstractMenu.java
@@ -1,22 +1,20 @@
-package moviereservation;
-
 import java.util.Scanner;
 
-abstract class AbstractMenu implements  Menu {
-     //기본 문구
-     String menuText;
-     Menu prevMenu;
+abstract class AbstractMenu implements Menu {
+    protected String menuText; // 기본 문구
+    protected Menu prevMenu; // 이전 메뉴
+    protected static final Scanner scanner = new Scanner(System.in);
 
-     static final Scanner scanner = new Scanner(System.in);
+    public AbstractMenu(String menuText, Menu prevMenu) { // 생성자
+        this.menuText = menuText;
+        this.prevMenu = prevMenu;
+    }
 
-     public AbstractMenu(String menuText, Menu prevMenu){
-         this.menuText = menuText;
-         this.prevMenu = prevMenu;
-     }
-     public void print(){
-         System.out.println("\n"+menuText );
-     }
-     public void setPrevMenu(Menu prevMenu){
-         this.prevMenu = prevMenu;
-     }
+    public void print() {
+        System.out.print("\n" + menuText); // 메뉴 출력
+    }
+
+    public void setPrevMenu(Menu prevMenu) { // 세터 메소드
+        this.prevMenu = prevMenu;
+    }
 }
