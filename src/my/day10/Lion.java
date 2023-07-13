44336afe44e988c688584283c43ec478de86d7b0
heegyun <yhg1124@daum.net>
2023-07-13 17:04:02 +0900

    Delete Lion.java

--- src/my/day10/Lion.java
+++
@@ -1,9 +0,0 @@
-package my.day10;
-
-// 2. 동물 클래스의 자식 클래스 (상속관계)
-public class Lion  extends Animal implements Predator {
-    @Override
-    public String getName() {
-        return name;
-    }
-}
